69b9741ae9d766adbacd5bdb7c80be9bd722b258
joshinils <34302971+joshinils@users.noreply.github.com>
2018-02-23 21:52:51 +0100

    initial commit
    changed necessary things to make it compatible with .16, added crude-oil recipe

---
+++ control.lua
@@ -0,0 +1,150 @@
+local tree={
+	"dead-dry-hairy-tree",-- 2
+	"dead-grey-trunk",    -- 2
+	"dead-tree-desert",          -- 2 nadelig, unschön
+	"dry-hairy-tree",     -- 2
+	"dry-tree",           -- 4
+--	"green-coral",        -- 1 ist halt ne koralle
+	"tree-01",            -- 4
+	"tree-02",            -- 4
+	"tree-02-red",        -- 4
+	"tree-03",            -- 4 sieht mau aus
+	"tree-04",            -- 4
+	"tree-05",            -- 4
+	"tree-06",            -- 4 hat was runterhängen hier drunter auch
+	"tree-06-brown",      -- 4
+	"tree-07",            -- 4
+	"tree-08",            -- 4
+	"tree-08-brown",      -- 4
+	"tree-08-red",        -- 4
+	"tree-09",            -- 4 rot
+	"tree-09-brown",      -- 4
+	"tree-09-red"         -- 4 rot
+}
+
+script.on_event(defines.events.on_built_entity, function(event)
+	if event.stack.name~="seedling" then
+		return
+	end
+	
+	pos=event.created_entity.position
+	event.created_entity.destroy()
+	
+	game.surfaces.nauvis.create_entity({
+		name=tree[math.floor(math.random()*21+1)], -- randomly from [1, 21]
+		position=pos
+	})
+	
+end)
+
+local radius=100 -- size of starting circle
+local tile = {} -- what each tile gets replaced with
+--tile["concrete"]="water",
+tile["deepwater"]="deepwater"
+tile["deepwater-green"]="deepwater-green"
+tile["dirt-1"]="water"
+tile["dirt-2"]="water"
+tile["dirt-3"]="water"
+tile["dirt-4"]="water"
+tile["dirt-5"]="water"
+tile["dirt-6"]="water"
+tile["dirt-7"]="water"
+tile["dry-dirt"]="deepwater"
+tile["grass-1"]="water"
+tile["grass-2"]="water"
+tile["grass-3"]="water"
+tile["grass-4"]="water"
+tile["hazard-concrete-left"]="water"
+tile["hazard-concrete-right"]="water"
+tile["lab-dark-1"]="water"
+tile["lab-dark-2"]="water"
+tile["out-of-map"]="water"
+tile["red-desert-0"]="water"
+tile["red-desert-1"]="water"
+tile["red-desert-2"]="water"
+tile["red-desert-3"]="water"
+tile["sand-1"]="water"
+tile["sand-2"]="water"
+tile["sand-3"]="water"
+tile["stone-path"]="water"
+tile["water"]="water"
+tile["water-green"]="water"
+
+
+-- inspired by "Water World", "author": "FuzzCat927"
+script.on_event(defines.events.on_chunk_generated, function(event)
+
+    area=event.area
+    surface=event.surface
+	--local contains_0 = false
+    local tiles={}
+	--game.print(tprint(event.area))
+	if area.left_top.x==0 and area.left_top.y==0 then
+		local chest = surface.create_entity({name = "wooden-chest", position={0,0}, force = game.forces.neutral})
+		chest.insert{name="seedling", count=1}
+		chest.insert{name="offshore-pump", count = 1}
+		chest.insert{name="assembling-machine-2", count=1}
+		chest.insert{name="landfill", count=42}
+		chest.insert{name="small-electric-pole", count=1}
+		chest.insert{name="solar-panel", count=1}
+		chest.insert{name="storage-tank", count=1}
+	end
+	
+	for x=area.left_top.x, area.right_bottom.x do
+		for y=area.left_top.y, area.right_bottom.y do
+			if x*x+y*y<radius then
+				for _, entity in ipairs(game.surfaces[1].find_entities_filtered{area = {{-40, -40}, {40, 40}}, type= "resource"}) do -- gets all resources in the rectangle
+					entity.destroy() --delete all ores
+				end
+				for _, entity in ipairs(game.surfaces[1].find_entities_filtered{area = {{-40, -40}, {40, 40}}, type= "tree"}) do -- gets all resources in the rectangle
+					entity.destroy() --delete all trees
+				end
+				table.insert(tiles, {name="grass-1", position={x, y}})
+			else
+				--table.insert(tiles, {name=tile[surface.get_tile(x,y).name], position={x, y}})
+				table.insert(tiles, {name="water", position={x, y}})
+			end
+		end
+	end
+
+	--surface.set_tiles(tiles, contains_0)
+	surface.set_tiles(tiles)
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+	player = game.players[event.player_index]
+	player.clear_items_inside()
+	player.insert{name="steel-axe", count=1, durability=100}
+	player.force.set_spawn_position({x=10,y=1}, player.surface) 
+end)
+
+-- spawn all trees:
+--/c tree={"dead-dry-hairy-tree", "dead-grey-trunk", "dead-tree", "dry-hairy-tree", "dry-tree", "green-coral", "tree-01", "tree-02", "tree-02-red", "tree-03", "tree-04", "tree-05", "tree-06", "tree-06-brown", "tree-07", "tree-08", "tree-08-brown", "tree-08-red", "tree-09", "tree-09-brown", "tree-09-red"}pos=game.player.position for i=1, 22 do game.surfaces.nauvis.create_entity({name=tree[i], position={pos.x+2*i, pos.y}})end
+
+
+-- taken from: https://stackoverflow.com/questions/41942289/display-contents-of-tables-in-lua
+-- useful for debugging
+function tprint (tbl, indent)
+	if not indent then indent = 0 end
+	local toprint = string.rep(" ", indent) .. "{\r\n"
+	indent = 0--indent + 2 
+	for k, v in pairs(tbl) do
+		toprint = toprint .. string.rep(" ", indent)
+		if (type(k) == "number") then
+			toprint = toprint .. "[" .. k .. "] = "
+		elseif (type(k) == "string") then
+			toprint = toprint	.. k ..	"= "	 
+		end
+		if (type(v) == "number") then
+			toprint = toprint .. v .. ", \r\n"
+		elseif (type(v) == "string") then
+			toprint = toprint .. "\"" .. v .. "\", \r\n"
+		elseif (type(v) == "table") then
+			toprint = toprint .. tprint(v, indent + 2) .. ", \r\n"
+		else
+			toprint = toprint .. "\"" .. tostring(v) .. "\", \r\n"
+		end
+	end
+	toprint = toprint .. string.rep(" ", indent-2) .. "}"
+	return toprint
+end
---
+++ data.lua
@@ -0,0 +1,373 @@
+data:extend{
+	{
+		type="tree",
+		name="sprout",
+		flags={"placeable-neutral","player-creation","placeable-off-grid"},
+		icon="__island_spawn__/seedling.png",
+		icon_size=32,
+		pictures={sheet={
+			filename="__island_spawn__/sprout.png",
+			frame_count=1,
+			width=39,
+			height=64,
+			variation_count=1
+		}},
+		collision_box = {{-1, -1}, {1, 1}}
+	},
+	{
+		type="item",
+		name="seedling",
+		icon="__island_spawn__/seedling.png",
+		icon_size=32,
+		flags={"goes-to-quickbar"},
+		subgroup="sludge-recipe",
+		order=" ",
+		place_result="sprout",
+		stack_size=10
+	},
+	{
+		type="recipe",
+		name="seedling-from-wood",
+		category='crafting',
+		energy_required=0.1,
+		ingredients={{"raw-wood", 1}},
+		result="seedling",
+		result_count=1,
+		enabled=true
+	},
+	{
+		type="fluid",
+		name="sludge",
+		default_temperature=25,
+		max_temperature=100,
+		heat_capacity="0.2KJ",
+		base_color={r=139, g=69, b=19},
+		flow_color={r=160, g=82, b=45},
+		icon="__island_spawn__/sludge.png",
+		icon_size=32,
+		order="a[fluid]-a[water]",
+		pressure_to_speed_ratio=0.4,
+		flow_to_energy_ratio=0.59
+	},
+	{
+		type="item-subgroup",
+		name="sludge-recipe",
+		order="a",
+		group="intermediate-products"
+	},
+	{
+		type='recipe',
+		name='sludge-creation',
+		category='crafting-with-fluid',
+		energy_required=3,
+		ingredients={
+			{type="fluid", name="water", amount=25}, 
+			{"raw-wood", 5}
+		},
+		results={{type="fluid", name="sludge", amount=30}},
+		enabled=true
+	}, -- 1 offshore pump produces 1200 water/sec
+	{
+		type='recipe',
+		name='stone-from-sludge',
+		subgroup="sludge-recipe",
+		main_product="",
+		category='crafting-with-fluid',
+		energy_required=3/2,
+		ingredients={{type="fluid", name="sludge", amount=10}},
+		icon="__island_spawn__/sludge-stone.png",
+		icon_size=32,
+		result="stone",
+		result_count=40,
+		order="d",
+		enabled=true
+	},
+	{
+		type='recipe',
+		name='iron-from-sludge',
+		subgroup="sludge-recipe",
+        main_product="",
+		category='crafting-with-fluid',
+		energy_required=3/2,
+		ingredients={{type="fluid", name="sludge", amount=10}},
+		icon="__island_spawn__/sludge-iron.png",
+		icon_size=32,
+		result="iron-ore",
+		result_count=40,
+		order="a",
+		enabled=true
+	},
+	{
+		type='recipe',
+		name='copper-from-sludge',
+		subgroup="sludge-recipe",
+        main_product="",
+		category='crafting-with-fluid',
+		energy_required=3/2,
+		ingredients={{type="fluid", name="sludge", amount=10}},
+		icon="__island_spawn__/sludge-copper.png",
+		icon_size=32,
+		result="copper-ore",
+		result_count=40,
+		order="b",
+		enabled=true
+	},
+	{
+		type='recipe',
+		name='coal-from-sludge',
+		main_product="",
+		subgroup="sludge-recipe",
+		category='crafting-with-fluid',
+		energy_required=3/2,
+		ingredients={{type="fluid", name="sludge", amount=10}},
+		icon="__island_spawn__/sludge-coal.png",
+		icon_size=32,
+		result="coal",
+		result_count=40,
+		order="c",
+		enabled=true
+	},
+	{
+		type='recipe',
+	    category="chemistry",
+		subgroup="sludge-recipe",
+		name='uranium-from-sludge',
+        main_product="",
+		energy_required=3/2,
+		ingredients={{type="fluid", name="sludge", amount=10},
+					 {type="fluid", name="sulfuric-acid", amount=40}}, --[[as much as there is ore produced]]
+		icon="__island_spawn__/sludge-uranium.png",
+		icon_size=32,
+		result="uranium-ore",
+		result_count=40,
+		order="e",
+		enabled=true
+	},
+	{
+		type="item-subgroup",
+		name="stone-processing",
+		order="b",
+		group="intermediate-products"
+	},
+	{
+		type="item",
+		name="gravel",
+		subgroup="stone-processing",
+		flags={},
+		order="a-r",
+		icon="__island_spawn__/gravel.png",
+		icon_size=32,
+		stack_size=50,
+		enabled=true
+	},
+	{
+		type="recipe",
+		category="crafting",
+		name="gravel-making",
+		subgroup="stone-processing",
+		energy_required=1,
+		ingredients={{"stone", 2}},
+		icon="__island_spawn__/gravel.png",
+		icon_size=32,
+		result="gravel",
+		result_count=1,
+		order="a-r",
+		enabled=false
+	},
+	{
+		type="item",
+		name="sand",
+		subgroup="stone-processing",
+		flags={},
+		order="b-r",
+		icon="__island_spawn__/sand.png",
+		icon_size=32,
+		stack_size=50,
+		enabled=true
+	},
+	{
+		type="recipe",
+		category="crafting",
+		name="sand-making",
+		subgroup="stone-processing",
+		energy_required=1,
+		ingredients={{"gravel", 2}},
+		icon="__island_spawn__/sand.png",
+		icon_size=32,
+		result="sand",
+		result_count=1,
+		order="b-r",
+		enabled=false
+	},
+	{
+		type="item",
+		name="dust",
+		subgroup="stone-processing",
+		flags={},
+		order="c-r",
+		icon="__island_spawn__/dust.png",
+		icon_size=32,
+		stack_size=50,
+		enabled=true
+	},
+	{
+		type="recipe",
+		category="crafting",
+		name="dust-making",
+		subgroup="stone-processing",
+		energy_required=1,
+		ingredients={{"sand", 2}},
+		icon="__island_spawn__/dust.png",
+		icon_size=32,
+		result="dust",
+		result_count=1,
+		order="c-r",
+		enabled=false
+	},
+	{
+		type="recipe",
+		name="gravel-sludge",
+		icon="__island_spawn__/sludge.png",
+		icon_size=32,
+		main_product="",
+		category="crafting-with-fluid",
+		subgroup="stone-processing",
+		energy_required=3,
+		ingredients={
+			{type="fluid", name="water", amount=26}, 
+			{"raw-wood", 3},
+			{"gravel", 1}
+		},
+		results={{type="fluid", name="sludge", amount=30}},
+		order="a-s",
+		enabled=false
+	},
+	{
+		type="recipe",
+		name="sand-sludge",
+		icon="__island_spawn__/sludge.png",
+		icon_size=32,
+		main_product="",
+		category="crafting-with-fluid",
+		subgroup="stone-processing",
+		energy_required=3,
+		ingredients={
+			{type="fluid", name="water", amount=28}, 
+			{"raw-wood", 1},
+			{"sand", 1}
+		},
+		results={{type="fluid", name="sludge", amount=30}},
+		order="b-s",
+		enabled=false
+	},
+	{
+		type="recipe",
+		name="dust-sludge",
+		icon="__island_spawn__/sludge.png",
+		icon_size=32,
+		main_product="",
+		category="crafting-with-fluid",
+		subgroup="stone-processing",
+		energy_required=3,
+		ingredients={
+			{type="fluid", name="water", amount=29}, 
+			{"dust", 1}
+		},
+		results={{type="fluid", name="sludge", amount=30}},
+		order="c-s",
+		enabled=false
+	},
+	{
+		type="recipe",
+		name="wood-pressing",
+		icon="__island_spawn__/wood-pressing.png",
+		icon_size=40,
+--		main_product="",
+		category="crafting-with-fluid",
+--		subgroup="stone-processing",
+		energy_required=30,
+		ingredients={
+			{"raw-wood", 10},
+		},
+		results={{type="fluid", name="crude-oil", amount=50}},
+--		order="b-s",
+		enabled=true
+	},
+	{
+		type="technology",
+		name="stone-processing-1",
+		icon="__island_spawn__/stone-processing-one.png",
+		icon_size=128,
+		effects={
+			{
+				type="unlock-recipe",
+				recipe="gravel-making"
+			},
+			{
+				type="unlock-recipe",
+				recipe="gravel-sludge"
+			}
+		},
+		prerequisites = {"automation"},
+		unit={
+			count=1,
+			ingredients={{"science-pack-1", 0}},
+			time=1
+		}
+	},
+	{
+		type="technology",
+		name="stone-processing-2",
+		icon="__island_spawn__/stone-processing-two.png",
+		icon_size=128,
+		effects={
+			{
+				type="unlock-recipe",
+				recipe="sand-making"
+			},
+			{
+				type="unlock-recipe",
+				recipe="sand-sludge"
+			}
+		},
+		prerequisites = {"automation-2", "stone-processing-1"},
+		unit={
+			count=1,
+			ingredients={{"science-pack-2", 0}},
+			time=1
+		}
+	},
+	{
+		type="technology",
+		name="stone-processing-3",
+		icon="__island_spawn__/stone-processing-three.png",
+		icon_size=128,
+		effects={
+			{
+				type="unlock-recipe",
+				recipe="dust-making"
+			},
+			{
+				type="unlock-recipe",
+				recipe="dust-sludge"
+			}
+		},
+		prerequisites = {"automation-3","stone-processing-2"},
+		unit={
+			count=1,
+			ingredients={{"science-pack-3", 0}},
+			time=1
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
